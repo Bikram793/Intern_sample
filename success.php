a99f1af3706858fc1678f88e653d93025b383ca2
bikram7151 <66201082+Bikram793@users.noreply.github.com>
2023-05-23 10:53:18 +0530

    first

---
+++ success.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form - Success</title>
+</head>
+<body>
+    <h1>Success</h1>
+    <?php
+    $name = $_POST['name'];
+    echo "<p>Thank you for filling the form, $name!</p>";
+    ?>
+</body>
+</html>
